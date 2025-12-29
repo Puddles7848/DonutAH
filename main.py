6c59c7f4a4434eeead251b570227a42acb4e4340
Puddles7848 <puddles7848@gmail.com>
2025-12-29 18:28:24 +0630

    Removed 2 imports. mk_key now outputs JSON.

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import os
 import requests
 import json
-import pprint
-from typing import Optional
 
 
 # Define key functions
@@ -13,14 +11,13 @@ def new_key(): # Makes a new key
         key.write(input("Please enter you DonutSMP API key that you can obtain by joining and typing \"/api\".\n   >"))
 
 
-def get_page(type: Optional[str] = None): # Gets page 1 with the option to return a JSON or DICT
+def get_page(): # Gets page 1 with the option to return a JSON or DICT
     url = "https://api.donutsmp.net/v1/auction/transactions/1"
     headers = {
         "accept": "application/json",
         "Authorization": "Bearer "+key
     }
-    return json.loads(requests.get(url, headers=headers).text) # yay
-
+    return requests.get(url, headers=headers).text # yay
 
 if os.path.exists("key.txt") == False: # Check if file exists
     new_key # If it doesn't, run new_key
